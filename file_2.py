6c4b8f5ffae3bc60551f17fa581c541596e69744
Tatiana <d0ra@yandex.ru>
2022-11-29 23:26:49 +0500

    stage

--- file_2.py
+++ file_2.py
@@ -4,5 +4,7 @@ import numpy as np
 df = pd.read_csv("football_data.csv")
 clubs = st.multiselect('Show Player for clubs?', df['Club'].unique())
 nationalities = st.multiselect('Show Player from Nationalities?', df['Nationality'].unique())
+# Filter dataframe
 new_df = df[(df['Club'].isin(clubs)) & (df['Nationality'].isin(nationalities))]
+# write dataframe to screen
 st.write(new_df)
