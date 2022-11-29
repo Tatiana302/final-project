fa78897ed5e20af8a14e284abd14c38b8e2ac802
Tatiana <d0ra@yandex.ru>
2022-11-29 22:55:14 +0500

    ++1

---
+++ file_2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+df = pd.read_csv("football_data.csv")
+clubs = st.multiselect('Show Player for clubs?', df['Club'].unique())
+nationalities = st.multiselect('Show Player from Nationalities?', df['Nationality'].unique())
+new_df = df[(df['Club'].isin(clubs)) & (df['Nationality'].isin(nationalities))]
+st.write(new_df)
