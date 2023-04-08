6d791b091edd68e7280f464ac0e56292ea86515d
Tatiana302 <118453746+Tatiana302@users.noreply.github.com>
2023-04-08 15:05:45 +0500

    Update file_1.py

--- file_1.py
+++ file_1.py
@@ -1,14 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title('A Simple Streamlit Web App')
-name = st.text_input('Enter your name', '')
-st.write(f'Hello {name}!')
-x = st.slider('Select an integer x', 1, 100, 1)
-y = st.slider('Select an integer y', 1, 100, 1)
-df = pd.DataFrame({'Сложение': [x + y],
-                   'Вычитание': [x - y],
-                   'Умножение': [x * y],
-                   'Деление': [round(x / y, 2)]},
-                  index=['Результат'])
+# Заголовок страницы
+st.title("A Simple Streamlit Web App")
+
+# Получение имени пользователя и вывод приветствия
+name = st.text_input("Enter your name", "")
+st.write(f"Hello {name}!")
+
+# Получение значений слайдеров и создание DataFrame
+variable_x = st.slider("Select an integer X", 1, 100, 1)
+variable_y = st.slider("Select an integer Y", 1, 100, 1)
+result = {"Сложение": variable_x+variable_y,
+          "Вычитание": variable_x-variable_y,
+          "Умножение": variable_x*variable_y,
+          "Деление": round(variable_x/variable_y,2)}
+df = pd.DataFrame(result, index=["Результат"])
+
+# Вывод DataFrame
 st.write(df)
