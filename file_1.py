689d668bb498d80989297181e0eee124325fc12d
EkaterinaSeveryanka <varelovna@mail.ru>
2023-04-08 15:25:19 +0500

    Changing variable names by user Tatiana302:main

--- file_1.py
+++ file_1.py
@@ -11,10 +11,12 @@ st.write(f"Hello {name}!")
 # Получение значений слайдеров и создание DataFrame
 variable_x = st.slider("Select an integer X", 1, 100, 1)
 variable_y = st.slider("Select an integer Y", 1, 100, 1)
-result = {"Сложение": variable_x+variable_y,
-          "Вычитание": variable_x-variable_y,
-          "Умножение": variable_x*variable_y,
-          "Деление": round(variable_x/variable_y,2)}
+result = {
+    "Сложение": variable_x + variable_y,
+    "Вычитание": variable_x - variable_y,
+    "Умножение": variable_x * variable_y,
+    "Деление": round(variable_x / variable_y, 2),
+}
 df = pd.DataFrame(result, index=["Результат"])
 
 # Вывод DataFrame
